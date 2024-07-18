4a2b5b8142b3627aededb54e25b2aeaa5cd1cc8b
Jackson Argo <jackson@startree.ai>
2024-07-17 18:10:21 -0700

    ok not soo painful

--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ PLUGIN_ARCHIVE="${PLUGIN_NAME}.zip"
 
 npm run build
 mage -v
-npx @grafana/sign-plugin@latest --rootUrls https://pinot-grafana-demo.9itmgf.cp.s7e.startree.cloud
+npx @grafana/sign-plugin@latest --rootUrls https://pinot-grafana-demo.9itmgf.cp.s7e.startree.cloud,https://grafana.gcstest.graphiant.startree.cloud,https://grafana.gcsproduction.graphiant.startree.cloud,https://grafana.la32x9.cp.s7e.startree-dev.cloud
 
 mv "${PLUGIN_SRC}" "${PLUGIN_NAME}"
 zip -r ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_NAME}"
@@ -18,7 +18,7 @@ mv ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_ARCHIVE}"
 mv "${PLUGIN_NAME}" "${PLUGIN_SRC}"
 
 ## deployment
-kubectl --namespace cell-9itmgf-default cp startree-pinot-datasource.zip pinot-grafana-demo-0:/var/lib/grafana/plugins
-kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- rm -rf /var/lib/grafana/plugins/startree-pinot-datasource
-kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- unzip /var/lib/grafana/plugins/startree-pinot-datasource.zip -d /var/lib/grafana/plugins/
-kubectl --namespace cell-9itmgf-default delete pod pinot-grafana-demo-0
+#kubectl --namespace cell-9itmgf-default cp startree-pinot-datasource.zip pinot-grafana-demo-0:/var/lib/grafana/plugins
+#kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- rm -rf /var/lib/grafana/plugins/startree-pinot-datasource
+#kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- unzip /var/lib/grafana/plugins/startree-pinot-datasource.zip -d /var/lib/grafana/plugins/
+#kubectl --namespace cell-9itmgf-default delete pod pinot-grafana-demo-0
--- src/types/EditorMode.ts
+++ src/types/EditorMode.ts
@@ -2,3 +2,5 @@ export enum EditorMode {
   Builder = 'Builder',
   Code = 'Code',
 }
+
+export const DefaultEditorMode = EditorMode.Builder;
