fa010e3b119fc3e247d3a9c4ba174b21d0783ca0
Jackson Argo <jackson@startree.ai>
2024-07-29 15:01:54 -0400

    cleanup

--- package.sh
+++ package.sh
@@ -4,19 +4,31 @@ set -xe
 
 PLUGIN_NAME="startree-pinot-datasource"
 PLUGIN_SRC="dist"
-PLUGIN_ARCHIVE="${PLUGIN_NAME}-1.0.0.zip"
+PLUGIN_VER="1.0.1"
+PLUGIN_ARCHIVE="${PLUGIN_NAME}-${PLUGIN_VER}.zip"
 
-npm run build
-mage -v
-npx @grafana/sign-plugin@latest --rootUrls https://pinot-grafana-demo.9itmgf.cp.s7e.startree.cloud,https://grafana.gcstest.graphiant.startree.cloud,https://grafana.gcsproduction.graphiant.startree.cloud,https://grafana.la32x9.cp.s7e.startree-dev.cloud
+K8S_NAMESPACE="cell-9itmgf-default"
+K8S_POD="pinot-grafana-demo-0"
+CONTAINER_PLUGINS_PATH="/var/lib/grafana/plugins"
 
-mv "${PLUGIN_SRC}" "${PLUGIN_NAME}"
-zip -r ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_NAME}"
-mv ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_ARCHIVE}"
-mv "${PLUGIN_NAME}" "${PLUGIN_SRC}"
+build() {
+  npm run build
+  mage -v
+  npx @grafana/sign-plugin@latest --rootUrls https://pinot-grafana-demo.9itmgf.cp.s7e.startree.cloud,https://grafana.gcstest.graphiant.startree.cloud,https://grafana.gcsproduction.graphiant.startree.cloud,https://grafana.la32x9.cp.s7e.startree-dev.cloud
 
-## deployment
-kubectl --namespace cell-9itmgf-default cp startree-pinot-datasource-1.0.0.zip pinot-grafana-demo-0:/var/lib/grafana/plugins
-kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- rm -rf /var/lib/grafana/plugins/startree-pinot-datasource
-kubectl --namespace cell-9itmgf-default exec pinot-grafana-demo-0 -- unzip /var/lib/grafana/plugins/startree-pinot-datasource-1.0.0.zip -d /var/lib/grafana/plugins/
-kubectl --namespace cell-9itmgf-default delete pod pinot-grafana-demo-0
+  mv "${PLUGIN_SRC}" "${PLUGIN_NAME}"
+  zip -r ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_NAME}"
+  mv ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_ARCHIVE}"
+  mv "${PLUGIN_NAME}" "${PLUGIN_SRC}"
+}
+
+deploy() {
+  # Deploy for demo cluster
+  kubectl --namespace "${K8S_NAMESPACE}" cp "${PLUGIN_ARCHIVE}" "${K8S_POD}":"${CONTAINER_PLUGINS_PATH}"
+  kubectl --namespace "${K8S_NAMESPACE}" exec "${K8S_POD}" -- rm -rf "${CONTAINER_PLUGINS_PATH}/${PLUGIN_NAME}"
+  kubectl --namespace "${K8S_NAMESPACE}" exec "${K8S_POD}" -- unzip "${CONTAINER_PLUGINS_PATH}/${PLUGIN_ARCHIVE}" -d "${CONTAINER_PLUGINS_PATH}"
+  kubectl --namespace "${K8S_NAMESPACE}" delete pod "${K8S_POD}"
+}
+
+build
+deploy
