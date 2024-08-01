8d05c629c908165e29f810749353437c7f656e13
Jackson Argo <jackson@jacksonargo.com>
2024-08-01 14:47:59 -0400

    Release automation (#22)
    * add release automation
    
    * cleanup
    
    * cleanup

---
+++ deploy_demo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -xe
+
+PLUGIN_NAME="startree-pinot-datasource"
+PLUGIN_SRC="dist"
+PLUGIN_VER="1.0.1"
+PLUGIN_ARCHIVE="${PLUGIN_NAME}-${PLUGIN_VER}.zip"
+
+K8S_NAMESPACE="cell-9itmgf-default"
+K8S_POD="pinot-grafana-demo-0"
+CONTAINER_PLUGINS_PATH="/var/lib/grafana/plugins"
+
+build() {
+  npm run build
+  mage -v
+  npx @grafana/sign-plugin@latest --rootUrls https://**.cp.s7e.startree.cloud
+
+  mv "${PLUGIN_SRC}" "${PLUGIN_NAME}"
+  zip -r ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_NAME}"
+  mv ".${PLUGIN_ARCHIVE}-next" "${PLUGIN_ARCHIVE}"
+  mv "${PLUGIN_NAME}" "${PLUGIN_SRC}"
+}
+
+deploy() {
+  # Deploy for demo cluster
+  kubectl --namespace "${K8S_NAMESPACE}" cp "${PLUGIN_ARCHIVE}" "${K8S_POD}":"${CONTAINER_PLUGINS_PATH}"
+  kubectl --namespace "${K8S_NAMESPACE}" exec "${K8S_POD}" -- rm -rf "${CONTAINER_PLUGINS_PATH}/${PLUGIN_NAME}"
+  kubectl --namespace "${K8S_NAMESPACE}" exec "${K8S_POD}" -- unzip "${CONTAINER_PLUGINS_PATH}/${PLUGIN_ARCHIVE}" -d "${CONTAINER_PLUGINS_PATH}"
+  kubectl --namespace "${K8S_NAMESPACE}" delete pod "${K8S_POD}"
+}
+
+#build
+deploy
