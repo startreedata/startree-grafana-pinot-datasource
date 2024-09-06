63709a36ffea73190d564334d661a8b52d83018e
Jackson Argo <jackson@jacksonargo.com>
2024-09-06 14:34:31 -0400

    Debounce text fields (#40)
    * fixup template
    
    * cleanup

--- src/components/QueryEditor/InputTextField.tsx
+++ src/components/QueryEditor/InputTextField.tsx
@@ -1,22 +1,41 @@
 import { FormLabel } from './FormLabel';
 import { Input } from '@grafana/ui';
 import { styles } from '../../styles';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
-export function InputTextField(props: {
+const DefaultDelayMs = 300;
+
+export function InputTextField({
+  current,
+  delayMs,
+  invalid,
+  labels: { label, tooltip },
+  onChange,
+  placeholder,
+}: {
   current: string | undefined;
   labels: { label: string; tooltip: string };
+  delayMs?: number;
+  invalid?: boolean;
+  placeholder?: string;
   onChange: (val: string) => void;
 }) {
-  const { current, labels, onChange } = props;
+  const [value, setValue] = useState<string | undefined>(current);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => value && current !== value && onChange(value), delayMs || DefaultDelayMs);
+    return () => clearTimeout(timeoutId);
+  }, [value, current, onChange, delayMs]);
 
   return (
     <div className={'gf-form'}>
-      <FormLabel tooltip={labels.tooltip} label={labels.label} />
+      <FormLabel tooltip={tooltip} label={label} />
       <Input
         className={`${styles.QueryEditor.inputForm}`}
-        onChange={(event: ChangeEvent<HTMLInputElement>) => onChange(event.target.value)}
-        value={current}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setValue(event.target.value)}
+        invalid={invalid}
+        placeholder={placeholder}
+        value={value}
       />
     </div>
   );
