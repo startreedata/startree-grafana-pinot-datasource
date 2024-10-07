9c916a23316abea91ceb311994c69813216241bf
“anil-velotio” <anil.kumar@thriveworks.com>
2024-10-07 17:17:35 +0530

    [STP-3436] - time series should render on switch builder to code and back to builder

--- src/components/QueryEditor/ConfirmModal.tsx
+++ src/components/QueryEditor/ConfirmModal.tsx
@@ -20,7 +20,7 @@ export function ConfirmModal({ isOpen, onCancel, onDiscard, onCopy }: ConfirmMod
   return (
     <Modal
       title={
-        <div className="modal-header-title">
+        <div className="modal-header-title" data-testid="modal-header-title">
           <Icon name="exclamation-triangle" size="lg" />
           <span className="p-l-1">Warning</span>
         </div>
@@ -28,19 +28,28 @@ export function ConfirmModal({ isOpen, onCancel, onDiscard, onCopy }: ConfirmMod
       onDismiss={onCancel}
       isOpen={isOpen}
     >
-      <p>
-        Builder mode does not display changes made in code. The query builder will display the last changes you made in
-        builder mode.
-      </p>
-      <p>Do you want to copy your code to the clipboard?</p>
+      <div data-testid="modal-body">
+        <p>
+          Builder mode does not display changes made in code. The query builder will display the last changes you made
+          in builder mode.
+        </p>
+        <p>Do you want to copy your code to the clipboard?</p>
+      </div>
+
       <Modal.ButtonRow>
-        <Button type="button" variant="secondary" onClick={onCancel} fill="outline">
+        <Button type="button" variant="secondary" onClick={onCancel} fill="outline" data-testid="cancel-btn">
           Cancel
         </Button>
-        <Button variant="destructive" type="button" onClick={onDiscard} ref={buttonRef}>
+        <Button
+          variant="destructive"
+          type="button"
+          onClick={onDiscard}
+          ref={buttonRef}
+          data-testid="discard-code-and-switch-btn"
+        >
           Discard code and switch
         </Button>
-        <Button variant="primary" onClick={onCopy}>
+        <Button variant="primary" onClick={onCopy} data-testid="copy-code-and-switch-btn">
           Copy code and switch
         </Button>
       </Modal.ButtonRow>
