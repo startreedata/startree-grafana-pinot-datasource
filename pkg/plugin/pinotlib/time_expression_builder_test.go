d9f46806b3dc81140193684ca64fd5cb9c5b198c
Jackson Argo <jackson@startree.ai>
2024-10-11 16:19:34 -0400

    filter to uses < instead of <=

--- pkg/plugin/pinotlib/time_expression_builder_test.go
+++ pkg/plugin/pinotlib/time_expression_builder_test.go
@@ -20,15 +20,141 @@ func TestTimeExpressionBuilder_TimeFilterExpr(t *testing.T) {
 	require.NoError(t, err)
 
 	got := exprBuilder.TimeFilterExpr(time.Unix(1, 0), time.Unix(3601, 0))
-	assert.Equal(t, `"time" >= 1 AND "time" <= 3601`, got)
+	assert.Equal(t, `"time" >= 1 AND "time" < 3601`, got)
 }
 
 func TestTimeExpressionBuilder_TimeFilterBucketAlignedExpr(t *testing.T) {
 	exprBuilder, err := NewTimeExpressionBuilder("time", "1:SECONDS:EPOCH")
 	require.NoError(t, err)
 
-	got := exprBuilder.TimeFilterBucketAlignedExpr(time.Unix(1, 0), time.Unix(3601, 0), time.Minute)
-	assert.Equal(t, `"time" >= 0 AND "time" <= 3660`, got)
+	testArgs := []struct {
+		name        string
+		from        time.Time
+		to          time.Time
+		granularity time.Duration
+		want        string
+	}{
+		{
+			name:        "from=0,to=3599,granularity=millisecond",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3599, 0),
+			granularity: time.Millisecond,
+			want:        `"time" >= 0 AND "time" < 3599`,
+		},
+		{
+			name:        "from=0,to=3599,granularity=second",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3599, 0),
+			granularity: time.Second,
+			want:        `"time" >= 0 AND "time" < 3599`,
+		},
+		{
+			name:        "from=0,to=3599,granularity=minute",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3599, 0),
+			granularity: time.Minute,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=0,to=3599,granularity=hour",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3599, 0),
+			granularity: time.Hour,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=0,to=3600,granularity=millisecond",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Millisecond,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=0,to=3600,granularity=second",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Second,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=0,to=3600,granularity=minute",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Minute,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=0,to=3600,granularity=hour",
+			from:        time.Unix(0, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Hour,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=1,to=3600,granularity=millisecond",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Millisecond,
+			want:        `"time" >= 1 AND "time" < 3600`,
+		},
+		{
+			name:        "from=1,to=3600,granularity=second",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Second,
+			want:        `"time" >= 1 AND "time" < 3600`,
+		},
+		{
+			name:        "from=1,to=3600,granularity=minute",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Minute,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=1,to=3600,granularity=hour",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3600, 0),
+			granularity: time.Hour,
+			want:        `"time" >= 0 AND "time" < 3600`,
+		},
+		{
+			name:        "from=1,to=3601,granularity=millisecond",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3601, 0),
+			granularity: time.Millisecond,
+			want:        `"time" >= 1 AND "time" < 3601`,
+		},
+		{
+			name:        "from=1,to=3601,granularity=second",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3601, 0),
+			granularity: time.Second,
+			want:        `"time" >= 1 AND "time" < 3601`,
+		},
+		{
+			name:        "from=1,to=3601,granularity=minute",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3601, 0),
+			granularity: time.Minute,
+			want:        `"time" >= 0 AND "time" < 3660`,
+		},
+		{
+			name:        "from=1,to=3601,granularity=hour",
+			from:        time.Unix(1, 0),
+			to:          time.Unix(3601, 0),
+			granularity: time.Hour,
+			want:        `"time" >= 0 AND "time" < 7200`,
+		},
+	}
+
+	for _, tt := range testArgs {
+		t.Run(tt.name, func(t *testing.T) {
+			got := exprBuilder.TimeFilterBucketAlignedExpr(tt.from, tt.to, tt.granularity)
+			assert.Equal(t, tt.want, got)
+
+		})
+	}
 }
 
 func TestTimeExpressionBuilder_TimeExpr(t *testing.T) {
