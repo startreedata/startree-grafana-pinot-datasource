7689630b5112b0e5ef760fc3e6455d0e2a7c3d3a
Jackson Argo <jackson@startree.ai>
2024-10-21 13:12:39 -0400

    add a summary for collapsed panels

--- src/datasource.ts
+++ src/datasource.ts
@@ -4,6 +4,8 @@ import { DataSourceWithBackend } from '@grafana/runtime';
 import { GetDefaultPinotDataQuery, interpolateVariables, PinotDataQuery } from './types/PinotDataQuery';
 import { PinotConnectionConfig } from './types/PinotConnectionConfig';
 import { PinotVariableSupport } from './variables';
+import { QueryType } from './types/QueryType';
+import { EditorMode } from './types/EditorMode';
 
 export class DataSource extends DataSourceWithBackend<PinotDataQuery, PinotConnectionConfig> {
   constructor(instanceSettings: DataSourceInstanceSettings<PinotConnectionConfig>) {
@@ -14,6 +16,27 @@ export class DataSource extends DataSourceWithBackend<PinotDataQuery, PinotConne
 
   getDefaultQuery(_: CoreApp): Partial<PinotDataQuery> {
     return GetDefaultPinotDataQuery();
+  }
+
+  getQueryDisplayText(query: PinotDataQuery): string {
+    switch (query.queryType) {
+      case QueryType.PromQL:
+        return query.promQlCode || 'Empty query';
+      case QueryType.PinotQL:
+        switch (query.editorMode) {
+          case EditorMode.Code:
+            return query.pinotQlCode || 'Empty query';
+          case EditorMode.Builder:
+            const filters =
+              query.filters?.map((f) => `${f.columnName} ${f.operator} ${f.valueExprs?.join(',')}`).join(',') || 'none';
+            const dims = query.groupByColumns?.join(',') || 'none';
+            return `Table: ${query.tableName}, Time: ${query.timeColumn}, Aggregation: ${query.aggregationFunction}, Metric: ${query.metricColumn}, Dimensions: ${dims} Filters: ${filters}`;
+          default:
+            return 'Empty query';
+        }
+      default:
+        return 'Empty query';
+    }
   }
 
   applyTemplateVariables(
