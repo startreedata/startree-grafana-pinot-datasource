7a7ce0fcf7da547b920c6ecc3d8aeeaa046c5f0b
“anil-velotio” <anil.kumar@thriveworks.com>
2024-11-07 17:31:01 +0530

    Added e2e test for sql should work and return data

--- cypress/e2e/variable-query-editor-flow/variable-query-editor-flow.cy.ts
+++ cypress/e2e/variable-query-editor-flow/variable-query-editor-flow.cy.ts
@@ -633,4 +633,230 @@ describe('Add variable with Variable Query Editor', () => {
       });
     });
   });
+
+  it('Sql query should show the return data', () => {
+    /**
+     * All Intercepts
+     */
+    // Required for create and delete pinot datasource
+    cy.intercept('GET', '/api/datasources').as('getDatasources');
+    cy.intercept('GET', '/api/plugins?embedded=0').as('pluginsEmbedded');
+    cy.intercept('GET', '/api/plugins/errors').as('pluginsErrors');
+    cy.intercept('GET', '/api/gnet/plugins').as('gnetPlugins');
+    cy.intercept('GET', '/api/plugins?enabled=1&type=datasource').as('pluginsTypeDatasource');
+    cy.intercept('POST', '/api/datasources', (req) => {
+      req.continue((res) => (ctx.newlyCreatedDatasourceUid = res.body.datasource.uid));
+    }).as('postDatasources');
+    cy.intercept('GET', '/api/frontend/settings').as('frontendSettings');
+    cy.intercept('GET', '/api/access-control/user/permissions?reloadcache=true').as('userPermissions');
+    cy.intercept('GET', '/api/datasources/uid/*?accesscontrol=true').as('datasourcesUidAccessControl');
+    cy.intercept('GET', '/api/plugins/startree-pinot-datasource/settings').as('pluginsSettings');
+    cy.intercept('PUT', '/api/datasources/uid/*').as('datasourcesUid');
+    cy.intercept('GET', '/api/datasources/*/health').as('datasourcesHealth');
+    cy.intercept('DELETE', '/api/datasources/uid/*').as('deleteDatasource');
+    // Required for flow
+    cy.intercept('GET', '/api/datasources/*/resources/tables', (req) => {
+      req.continue((res) => (ctx.apiResponse.resourcesTables = res.body));
+    }).as('resourcesTables');
+    cy.intercept('POST', '/api/ds/query').as('dsQuery');
+    cy.intercept('GET', '/api/datasources/*/resources/tables/*/schema', (req) => {
+      req.continue((res) => (ctx.apiResponse.resourcesTablesSchema = res.body));
+    }).as('resourcesTablesSchema');
+
+    const formData = {
+      table: 'complex_website',
+      pinotQuery: `SELECT DISTINCT "browser" FROM "complex_website" ORDER BY "browser" ASC LIMIT 100;`,
+    };
+
+    /**
+     * Create new Pinot Datasource for testing create panel flow
+     */
+    cy.createPinotDatasource().then((data) => {
+      cy.wrap({
+        name: data.name,
+        uid: ctx.newlyCreatedDatasourceUid,
+      }).as('newlyCreatedDatasource');
+    });
+
+    /**
+     * Visit New Dashboard page
+     */
+    cy.visit('/dashboard/new');
+    cy.location('pathname').should('eq', '/dashboard/new');
+
+    /**
+     * Check and click Dashboard settings
+     */
+    cy.get('[aria-label="Dashboard settings"]').should('exist').click();
+    cy.location('search').should('contain', 'editview=settings');
+    cy.get('[aria-label="Search links"]').should('exist');
+
+    /**
+     * Check Variables link and click
+     */
+    cy.get('a').contains('Variables').click();
+    cy.location('search').should('contain', 'editview=templating');
+
+    cy.get('button[data-testid="data-testid Call to action button Add variable"]')
+      .should('exist')
+      .and('contain.text', 'Add variable')
+      .click();
+
+    cy.wait(['@resourcesTables', '@dsQuery']);
+    cy.get('form[aria-label="Variable editor Form"]').should('exist');
+
+    /**
+     * Check and select Data source
+     */
+    cy.get('@newlyCreatedDatasource').then((data: unknown) => {
+      const pinotDatasourceName: string = (data as any).name;
+
+      cy.get('[aria-label="Data source picker select container"]').should('exist').as('dataSourcePicker').click();
+
+      cy.get('[aria-label="Select options menu"]')
+        .should('be.visible')
+        .within(() => {
+          cy.contains(pinotDatasourceName).click();
+        });
+
+      // Check the selected data source
+      cy.get('@dataSourcePicker').should('contain.text', pinotDatasourceName);
+    });
+
+    /**
+     * Check Select Variable Type
+     */
+    cy.getBySel('select-variable-type')
+      .should('exist')
+      .within(() => {
+        cy.getBySel('inline-form-label').should('exist').and('have.text', 'Type');
+
+        // Check and select Sql Query radio button
+        cy.get('input[type="radio"]')
+          .should('exist')
+          .parent()
+          .within(() => {
+            cy.get('label').should('exist').contains('Sql Query').click();
+            cy.wait('@dsQuery');
+          });
+      });
+
+    /**
+     * Check initial Preview Values
+     */
+    cy.contains('Preview of values')
+      .parent()
+      .parent()
+      .as('previewOfValues')
+      .within(() => {
+        cy.get('label[aria-label="Variable editor Preview of Values option"]')
+          .should('exist')
+          .and('have.length', 1)
+          .contains('None');
+      });
+
+    /**
+     * Check and select Table
+     */
+    cy.getBySel('select-table')
+      .should('exist')
+      .within(() => {
+        cy.getBySel('inline-form-label').should('exist').and('have.text', 'Table');
+
+        // Check select list options
+        cy.get('input')
+          .parent()
+          .parent()
+          .as('tableSelect')
+          .within(() => {
+            cy.contains('Choose');
+          })
+          .click();
+
+        cy.wrap(cy.$$('body'))
+          .find('[aria-label="Select options menu"]')
+          .should('be.visible')
+          .within(() => {
+            const selectOptions = ctx.apiResponse.resourcesTables.tables as string[];
+            selectOptions.forEach((option) => cy.contains(option));
+
+            // Select the option
+            cy.contains(formData.table).click();
+            cy.wait(['@resourcesTablesSchema', '@dsQuery']);
+          });
+
+        // Check if correct option is selected
+        cy.get('@tableSelect').within(() => {
+          cy.contains(formData.table);
+        });
+      });
+
+    /**
+     * Check and fill Pinot Query
+     */
+    cy.getBySel('sql-editor')
+      .should('exist')
+      .within(() => {
+        cy.getBySel('inline-form-label').should('exist').and('have.text', 'Pinot Query');
+
+        // Check the sql editor
+        cy.get('[aria-label="Code editor container"]')
+          .should('exist')
+          .within(() => {
+            cy.get('.monaco-editor', { timeout: 5000 }).should('exist');
+
+            cy.window().then((win) => {
+              // Access the Monaco Editor instance via the window object
+              const editor = (win as any).monaco.editor.getModels()[0]; // Get the first model instance
+              const editorValue = editor.getValue(); // Retrieve the editor's content
+              console.log(editorValue);
+              cy.log(editorValue);
+
+              // Check the initial query value
+              cy.wrap(editorValue).should('be.empty');
+
+              // Set the new pinot query value
+              editor.setValue(formData.pinotQuery.trim());
+
+              // Check if query editor has the new value
+              const editorNewValue = editor.getValue();
+              cy.wrap(formData.pinotQuery.trim().replace(/ /g, '')).should(
+                'equal',
+                editorNewValue.trim().replace(/ /g, '')
+              );
+            });
+
+            cy.wait('@dsQuery').its('response.body').as('dsQueryResp');
+          });
+      });
+
+    /**
+     * Check Preview of values
+     */
+    cy.get('@dsQueryResp').then((resp: unknown) => {
+      const data = resp as Record<string, any>;
+      const previewValues: string[] = data.results.A.frames[0].data.values[0];
+
+      cy.get('@previewOfValues').within(() => {
+        cy.get('label[aria-label="Variable editor Preview of Values option"]').should('exist').as('previewValue');
+
+        // Check Preview values
+        previewValues.forEach((value) => {
+          cy.get('@previewValue').contains(value);
+        });
+      });
+    });
+
+    /**
+     * Delete the newly created data source for the panel
+     */
+    cy.get('@newlyCreatedDatasource').then((data: unknown) => {
+      const datasourceUid = (data as any).uid;
+      cy.deletePinotDatasourceWithUi(datasourceUid).then((result) => {
+        if (result.success) {
+          ctx.newlyCreatedDatasourceUid = null;
+        }
+      });
+    });
+  });
 });
