0cfd6abd49920bd0f64a7f0f74861f3ff17318d0
Jackson Argo <jackson@jacksonargo.com>
2024-11-13 14:00:26 -0500

    replace monaco promql editor with prom lens (#99)
    * almost there; just needs to filter suggestions
    
    * ok this is working very well
    
    * cleanup packages
    
    * remove unneeded monaco code
    
    * no need to lazy load

--- src/components/QueryEditor/QueryEditor.tsx
+++ src/components/QueryEditor/QueryEditor.tsx
@@ -1,27 +1,24 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { PinotQueryEditorProps } from '../../types/PinotQueryEditorProps';
 
 import { QueryEditorHeader } from './QueryEditorHeader';
 import { QueryType } from '../../types/QueryType';
-
-const PinotQlEditor = lazy(() => import('./PinotQlEditor').then((module) => ({ default: module.PinotQlEditor })));
-const PromQlEditor = lazy(() => import('./PromQlEditor').then((module) => ({ default: module.PromQlEditor })));
+import { PromQlEditor } from './PromQlEditor';
+import { PinotQlEditor } from './PinotQlEditor';
 
 export function QueryEditor(props: PinotQueryEditorProps) {
   return (
     <div>
       <QueryEditorHeader {...props} />
-      <Suspense fallback={null}>
-        {(() => {
-          switch (props.query.queryType) {
-            case QueryType.PromQL:
-              return <PromQlEditor {...props} />;
-            case QueryType.PinotQL:
-            default:
-              return <PinotQlEditor {...props} />;
-          }
-        })()}
-      </Suspense>
+      {(() => {
+        switch (props.query.queryType) {
+          case QueryType.PromQL:
+            return <PromQlEditor {...props} />;
+          case QueryType.PinotQL:
+          default:
+            return <PinotQlEditor {...props} />;
+        }
+      })()}
     </div>
   );
 }
