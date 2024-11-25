85236ca7b340458be26df93c26638fb830c1b406
Jackson Argo <jackson@startree.ai>
2024-11-25 12:24:35 -0500

    try this

--- pkg/plugin/test_helpers/test_helpers.go
+++ pkg/plugin/test_helpers/test_helpers.go
@@ -4,16 +4,23 @@ import (
 	"github.com/startreedata/startree-grafana-pinot-datasource/pkg/plugin/pinotlib"
 	"github.com/startreedata/startree-grafana-pinot-datasource/pkg/plugin/pinotlib/pinottest"
 	"github.com/stretchr/testify/require"
+	"sync"
 	"testing"
 )
 
-func SetupPinotAndCreateClient(t *testing.T) *pinotlib.PinotClient {
-	pinottest.CreateTestTables()
+var setupOnce sync.Once
+var pinotClient *pinotlib.PinotClient
 
-	pinotClient, err := pinotlib.NewPinotClient(pinotlib.PinotClientProperties{
-		ControllerUrl: pinottest.ControllerUrl,
-		BrokerUrl:     pinottest.BrokerUrl,
+func SetupPinotAndCreateClient(t *testing.T) *pinotlib.PinotClient {
+	setupOnce.Do(func() {
+		pinottest.CreateTestTables()
+
+		var err error
+		pinotClient, err = pinotlib.NewPinotClient(pinotlib.PinotClientProperties{
+			ControllerUrl: pinottest.ControllerUrl,
+			BrokerUrl:     pinottest.BrokerUrl,
+		})
+		require.NoError(t, err)
 	})
-	require.NoError(t, err)
 	return pinotClient
 }
