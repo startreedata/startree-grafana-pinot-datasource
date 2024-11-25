217571693843761e77f5150e36f4f24faa64e3ca
Jackson Argo <jackson@startree.ai>
2024-11-25 15:29:32 -0500

    try with no limits

--- pkg/plugin/pinotlib/pinot_client_test.go
+++ pkg/plugin/pinotlib/pinot_client_test.go
@@ -9,11 +9,11 @@ import (
 func setupPinotAndCreateClient(t *testing.T) *PinotClient {
 	pinottest.CreateTestTables()
 	pinotClient := NewPinotClient(PinotClientProperties{
-		ControllerUrl:              pinottest.ControllerUrl,
-		BrokerUrl:                  pinottest.BrokerUrl,
-		BrokerCacheTimeout:         time.Minute,
-		BrokerMaxConcurrentQueries: 1,
-		BrokerMaxQueryRate:         1 * time.Second,
+		ControllerUrl:      pinottest.ControllerUrl,
+		BrokerUrl:          pinottest.BrokerUrl,
+		BrokerCacheTimeout: time.Minute,
+		//BrokerMaxConcurrentQueries: 1,
+		//BrokerMaxQueryRate:         1 * time.Second,
 	})
 	return pinotClient
 }
