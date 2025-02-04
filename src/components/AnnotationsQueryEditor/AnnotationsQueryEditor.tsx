ca8041d8b2f48de1447ce28358cdc86294d07f65
Jackson Argo <jackson@jacksonargo.com>
2025-02-04 17:25:40 -0500

    annotation query fixups (#129)
    * annotation query fixups
    
    * fixup tests

--- src/components/AnnotationsQueryEditor/AnnotationsQueryEditor.tsx
+++ src/components/AnnotationsQueryEditor/AnnotationsQueryEditor.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { PinotQueryEditorProps } from '../../dataquery/PinotQueryEditorProps';
+import { PinotQlCode } from '../QueryEditor/PinotQlCode';
+import { CodeQuery } from '../../pinotql';
+import { interpolateVariables } from '../../dataquery/PinotDataQuery';
 import { DisplayType } from '../../dataquery/DisplayType';
-import { QueryEditor } from '../QueryEditor/QueryEditor';
-import { EditorMode } from '../../dataquery/EditorMode';
+
+const DefaultAnnotationsQuery =
+  //language=text
+  `SELECT
+  $__timeGroup("timestamp") AS "time",
+  'My annotation' as "title",
+  'My annotation text' as "text"
+FROM $__table()
+WHERE $__timeFilter("timestamp")
+GROUP BY "time"
+LIMIT 100000`;
 
 export function AnnotationsQueryEditor(props: PinotQueryEditorProps) {
-  props.query.displayType = DisplayType.ANNOTATIONS;
-  props.query.editorMode = EditorMode.Code;
-  return <QueryEditor {...props} />;
+  const savedParams = CodeQuery.paramsFrom(props.query);
+  savedParams.displayType = DisplayType.ANNOTATIONS;
+  if (!savedParams.pinotQlCode) {
+    savedParams.pinotQlCode = DefaultAnnotationsQuery;
+  }
+
+  return (
+    <PinotQlCode
+      datasource={props.datasource}
+      query={props.query}
+      timeRange={{ to: props.range?.to, from: props.range?.from }}
+      intervalSize={props.data?.request?.interval}
+      savedParams={savedParams}
+      interpolatedParams={CodeQuery.paramsFrom(interpolateVariables(props.query, props.data?.request?.scopedVars))}
+      onChange={(params) => props.onChange(CodeQuery.dataQueryOf(props.query, params))}
+      onRunQuery={props.onRunQuery}
+    />
+  );
 }
