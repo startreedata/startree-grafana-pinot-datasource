2b408269edfc037a9d517892d3033cf3c60cf7a1
Soumya Himanish Mohapatra <30361728+himanish-star@users.noreply.github.com>
2025-10-31 13:36:55 +0530

    removal

--- pkg/plugin/dataquery/data_response.go
+++ pkg/plugin/dataquery/data_response.go
@@ -1,10 +1,11 @@
 package dataquery
 
 import (
+	"net/http"
+
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/startreedata/startree-grafana-pinot-datasource/pkg/pinot"
-	"net/http"
 )
 
 func NewEmptyDataResponse() backend.DataResponse {
@@ -28,41 +29,38 @@ func NewOkDataResponse(frames ...*data.Frame) backend.DataResponse {
 
 func NewPartialDataResponse(frames []*data.Frame, exceptions []pinot.BrokerException) backend.DataResponse {
 	return backend.DataResponse{
-		Status:      backend.StatusInternal,
-		Frames:      frames,
-		Error:       pinot.NewBrokerExceptionError(exceptions),
-		ErrorSource: backend.ErrorSourceDownstream,
+		Status: backend.StatusInternal,
+		Frames: frames,
+		Error:  backend.DownstreamError(pinot.NewBrokerExceptionError(exceptions)),
 	}
 }
 
 func NewPinotExceptionsDataResponse(exceptions []pinot.BrokerException) backend.DataResponse {
 	return backend.DataResponse{
-		Status:      backend.StatusInternal,
-		Error:       pinot.NewBrokerExceptionError(exceptions),
-		ErrorSource: backend.ErrorSourceDownstream,
+		Status: backend.StatusInternal,
+		Error:  backend.DownstreamError(pinot.NewBrokerExceptionError(exceptions)),
 	}
 }
 
 func NewBadRequestErrorResponse(err error) backend.DataResponse {
-	return NewErrorDataResponse(backend.StatusBadRequest, err, backend.ErrorSourcePlugin)
+	return NewErrorDataResponse(backend.StatusBadRequest, err)
 }
 
 func NewPluginErrorResponse(err error) backend.DataResponse {
-	return NewInternalErrorDataResponse(err, backend.ErrorSourcePlugin)
+	return NewInternalErrorDataResponse(err)
 }
 
 func NewDownstreamErrorResponse(err error) backend.DataResponse {
-	return NewInternalErrorDataResponse(err, backend.ErrorSourceDownstream)
+	return NewInternalErrorDataResponse(backend.DownstreamError(err))
 }
 
-func NewInternalErrorDataResponse(err error, source backend.ErrorSource) backend.DataResponse {
-	return NewErrorDataResponse(backend.StatusInternal, err, source)
+func NewInternalErrorDataResponse(err error) backend.DataResponse {
+	return NewErrorDataResponse(backend.StatusInternal, err)
 }
 
-func NewErrorDataResponse(status backend.Status, err error, source backend.ErrorSource) backend.DataResponse {
+func NewErrorDataResponse(status backend.Status, err error) backend.DataResponse {
 	return backend.DataResponse{
-		Status:      status,
-		Error:       err,
-		ErrorSource: source,
+		Status: status,
+		Error:  err,
 	}
 }
