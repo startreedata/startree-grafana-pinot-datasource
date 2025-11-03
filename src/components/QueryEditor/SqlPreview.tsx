b9fd5417646f14813325ddab8ad5badab30162b6
Soumya Himanish Mohapatra <30361728+himanish-star@users.noreply.github.com>
2025-11-03 22:13:55 +0530

    copy button

--- src/components/QueryEditor/SqlPreview.tsx
+++ src/components/QueryEditor/SqlPreview.tsx
@@ -10,11 +10,13 @@ export function SqlPreview(props: { sql: string | undefined }) {
   return (
     <div className="gf-form" data-testid="sql-preview-container">
       <FormLabel tooltip={labels.tooltip} label={labels.label} />
-      <div style={{ position: 'relative', display: 'flex', alignItems: 'flex-start' }}>
+      <div style={{ position: 'relative' }}>
         <pre data-testid="sql-preview" style={{ margin: 0, flex: 1 }}>
           {sql || ''}
         </pre>
-        {sql && <CopyButton text={sql} />}
+        {sql && <div style={{ position: 'absolute', right: 4, bottom: 8 }}>
+          <CopyButton text={sql} />
+        </div>}
       </div>
     </div>
   );
